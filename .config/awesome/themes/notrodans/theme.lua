a7a06a5e6d8d01b54a1af8d9d7a4b695e1f88760
notrodans <notrodans@gmail.comm>
2023-03-27 14:31:18 +0300

    fix backgrounds

--- .config/awesome/themes/notrodans/theme.lua
+++ .config/awesome/themes/notrodans/theme.lua
@@ -71,7 +71,7 @@ theme.titlebar_maximized_button_focus_inactive = themes_path .. "titlebar/maximi
 theme.titlebar_maximized_button_normal_active = themes_path .. "titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active = themes_path .. "titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path .. "background.png"
+theme.wallpaper = themes_path .. "natural.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path .. "layouts/fairhw.png"
