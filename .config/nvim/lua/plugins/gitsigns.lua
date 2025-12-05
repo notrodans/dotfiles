c4f2752266548e0039939a3a4d199c49b294d80d
Dmytro Zhukov <notrodans@gmail.com>
2025-12-05 20:10:56 +0200

    config(nvim): customize gitsigns configuration
    - nvim: update NvChad version
    - nvim: create custom gitsigns config file with custom delete signs

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,12 +2,12 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = "User FilePost",
 	opts = function()
+		require("configs.gitsigns")
+
 		local gitsigns = require("gitsigns")
 
 		vim.keymap.set("n", "<leader>gd", function()
 			gitsigns.diffthis()
 		end, { desc = "Git diffthis" })
-
-		return require("nvchad.configs.gitsigns")
 	end,
 }
