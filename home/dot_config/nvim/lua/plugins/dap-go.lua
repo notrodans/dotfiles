d96b5b810300ef207294091e60eeff95ff5f1c1a
Dmytro Zhukov <notrodans@gmail.com>
2026-08-09 17:26:50 +0300

    refactor: nvim — extract Go DAP into standalone plugin, lazy keys
    Move the delve adapter and dap-go setup out of dap.lua into a new
    dap-go.lua spec, and convert the F2/F5/... keymaps to lazy.nvim keys
    so the plugin loads only on demand. dap.lua config now only wires up
    nvim-dap-virtual-text.

---
+++ home/dot_config/nvim/lua/plugins/dap-go.lua
@@ -0,0 +1,74 @@
+return {
+	"leoluz/nvim-dap-go",
+
+	dependencies = {
+		"mfussenegger/nvim-dap",
+	},
+
+	keys = {
+		{
+			"<F5>",
+			function()
+				require("dap").continue()
+			end,
+			ft = "go",
+			desc = "Continue Go debugging",
+		},
+		{
+			"<leader>Dt",
+			function()
+				require("dap-go").debug_test()
+			end,
+			ft = "go",
+			desc = "Debug Go test",
+		},
+		{
+			"<leader>Dl",
+			function()
+				require("dap-go").debug_last_test()
+			end,
+			ft = "go",
+			desc = "Debug last Go test",
+		},
+	},
+
+	config = function()
+		require("dap-go").setup({
+			delve = {
+				path = "dlv",
+				initialize_timeout_sec = 20,
+				port = "${port}",
+				args = {},
+				build_flags = "",
+				detached = vim.fn.has("win32") == 0,
+			},
+		})
+
+		local dap = require("dap")
+
+		-- Override dap-go's adapter to run the project-managed Delve.
+		dap.adapters.go = function(callback)
+			callback({
+				type = "server",
+				host = "127.0.0.1",
+				port = "${port}",
+
+				executable = {
+					command = "go",
+					args = {
+						"tool",
+						"dlv",
+						"dap",
+						"-l",
+						"127.0.0.1:${port}",
+					},
+					cwd = vim.fn.getcwd(),
+				},
+
+				options = {
+					initialize_timeout_sec = 20,
+				},
+			})
+		end
+	end,
+}
--- home/dot_config/nvim/lua/plugins/dap.lua
+++ home/dot_config/nvim/lua/plugins/dap.lua
@@ -2,90 +2,75 @@ return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
 		"theHamsta/nvim-dap-virtual-text",
-		"leoluz/nvim-dap-go",
 	},
-	lazy = true,
+	keys = {
+		{
+			"<F2>",
+			function()
+				require("dap").terminate()
+			end,
+			desc = "Stop debugging",
+		},
+		{
+			"<F5>",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Continue debugging",
+		},
+		{
+			"<F6>",
+			function()
+				require("dap").repl.open()
+			end,
+			desc = "Open REPL",
+		},
+		{
+			"<F7>",
+			function()
+				require("dap").run_to_cursor()
+			end,
+			desc = "Run debugging to cursor",
+		},
+		{
+			"<F10>",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Step over",
+		},
+		{
+			"<F11>",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Step into",
+		},
+		{
+			"<F12>",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Step out",
+		},
+		{
+			"<leader>Db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Toggle Debug breakpoint",
+		},
+		{
+			"<leader>DB",
+			function()
+				local condition = vim.fn.input("Breakpoint condition: ")
+				require("dap").set_breakpoint(condition)
+			end,
+			desc = "Toggle Debug conditional Breakpoint",
+		},
+	},
+
 	config = function()
 		require("nvim-dap-virtual-text").setup()
-
-		require("dap-go").setup({
-			delve = {
-				path = "dlv",
-				initialize_timeout_sec = 20,
-				port = "${port}",
-				args = {},
-				build_flags = {},
-				detached = vim.fn.has("win32") == 0,
-			},
-		})
-
-		local dap = require("dap")
-
-		dap.adapters.go = function(callback)
-			callback({
-				type = "server",
-				host = "127.0.0.1",
-				port = "${port}",
-				executable = {
-					command = "go",
-					args = {
-						"tool",
-						"dlv",
-						"dap",
-						"-l",
-						"127.0.0.1:${port}",
-					},
-					cwd = vim.fn.getcwd(),
-				},
-				options = {
-					initialize_timeout_sec = 20,
-				},
-			})
-		end
-
-		require("dap.ext.vscode").load_launchjs(nil, {
-			go = { "go" },
-		})
-
-		vim.keymap.set("n", "<F2>", function()
-			dap.terminate()
-		end, { desc = "Stop debugging" })
-
-		vim.keymap.set("n", "<F5>", function()
-			dap.continue()
-		end, { desc = "Continue debugging" })
-
-		vim.keymap.set("n", "<F6>", function()
-			dap.repl.open()
-		end, { desc = "Open REPL" })
-
-		vim.keymap.set("n", "<F7>", function()
-			dap.run_to_cursor()
-		end, { desc = "Run debugging to cursor" })
-
-		vim.keymap.set("n", "<F10>", function()
-			dap.step_over()
-		end, { desc = "Step over" })
-
-		vim.keymap.set("n", "<F11>", function()
-			dap.step_into()
-		end, { desc = "Step into" })
-
-		vim.keymap.set("n", "<F12>", function()
-			dap.step_out()
-		end, { desc = "Step out" })
-
-		vim.keymap.set("n", "<leader>Db", function()
-			dap.toggle_breakpoint()
-		end, {
-			desc = "Toggle Debug breakpoint",
-		})
-
-		vim.keymap.set("n", "<leader>DB", function()
-			local condition = vim.fn.input("Breakpoint condition: ")
-			dap.set_breakpoint(condition)
-		end, {
-			desc = "Toggle Debug conditional Breakpoint",
-		})
 	end,
 }
