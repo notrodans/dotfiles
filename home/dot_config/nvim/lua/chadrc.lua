f26b2cc9917b43b8cf0c57e5cc9e87820fcc94e4
notrodans <notrodans@gmail.com>
2026-08-13 18:07:02 +0300

    feat(nvim): switch to Monochrome theme

--- home/dot_config/nvim/lua/chadrc.lua
+++ home/dot_config/nvim/lua/chadrc.lua
@@ -2,7 +2,7 @@
 return {
 	base46 = {
 		---@diagnostic disable-next-line: assign-type-mismatch <my theme>
-		theme = "mountain",
+		theme = "monochrome",
 		transparency = false,
 		hl_override = {
 			CursorLine = { bg = "line" },
