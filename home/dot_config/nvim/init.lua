4bcf29a6a124fc33bacaa240b42b94cc90eb5ab3
Dmytro Zhukov <notrodans@gmail.com>
2026-08-22 00:21:01 +0300

    refactor(nvim): remove neovide-specific config

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -41,23 +41,6 @@ require("nvchad.autocmds")
 require("modules.buffer_close").setup()
 require("modules.adaptive_scrolloff").setup()
 
-if vim.g.neovide then
-	vim.g.neovide_scroll_animation_length = 0
-	vim.g.neovide_cursor_animation_length = 0.05
-	vim.g.neovide_refresh_rate = 144
-	vim.g.neovide_progress_bar_enabled = false
-	vim.cmd([[
-		" system clipboard
-		nmap <c-c> "+y
-		vmap <c-c> "+y
-		nmap <c-v> "+p
-		inoremap <c-v> <c-r>+
-		cnoremap <c-v> <c-r>+
-		" use <c-r> to insert original character without triggering things like auto-pairs
-		inoremap <c-r> <c-v>
-	]])
-end
-
 vim.schedule(function()
 	require("mappings")
 end)
