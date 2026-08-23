6c03fde67c29bfd83306c326539bb3c22e0ea630
notrodans <notrodans@gmail.com>
2026-08-23 23:21:14 +0300

    refactor(nvim): limit autotag to tag closing
    Configure nvim-ts-autotag to keep automatic closing enabled while disabling its paired-tag rename behavior. Linked tag renaming is now handled by Neovim's native LSP linkedEditingRange support when the attached language server advertises that capability.
    
    This narrows the plugin to functionality that Neovim core does not currently replace instead of maintaining two competing implementations of tag synchronization. Auto-close behavior remains unchanged, so typing new HTML-like tags does not lose the convenience provided by the plugin.

--- home/dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ home/dot_config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -1,5 +1,11 @@
 return {
 	"windwp/nvim-ts-autotag",
 	event = "VeryLazy",
-	config = true
+	opts = {
+		opts = {
+			enable_close = true,
+			enable_rename = false,
+			enable_close_on_slash = false,
+		},
+	},
 }
