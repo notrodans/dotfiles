cec73d5b403c24994a2cd27565232b3446721dd9
notrodans <notrodans@gmail.com>
2026-09-06 21:05:40 +0300

    fix(nvim): sync tabufline after fzf file actions

--- home/dot_config/nvim/lua/plugins/fzf.lua
+++ home/dot_config/nvim/lua/plugins/fzf.lua
@@ -6,6 +6,32 @@ return {
 		local fzf = require("fzf-lua")
 		local actions = fzf.actions
 
+		local function with_tabufline(action)
+			return function(selected, opts)
+				action(selected, opts)
+
+				vim.schedule(function()
+					local buf = vim.api.nvim_get_current_buf()
+
+					if not vim.api.nvim_buf_is_valid(buf) or not vim.bo[buf].buflisted then
+						return
+					end
+
+					local bufs = vim.t.bufs
+					if type(bufs) ~= "table" then
+						bufs = {}
+					end
+
+					if not vim.tbl_contains(bufs, buf) then
+						bufs[#bufs + 1] = buf
+						vim.t.bufs = bufs
+					end
+
+					vim.cmd.redrawtabline()
+				end)
+			end
+		end
+
 		local function workspace_cwd()
 			return vim.fn.getcwd(-1, -1)
 		end
@@ -154,10 +180,10 @@ return {
 			},
 			actions = {
 				files = {
-					["enter"] = actions.file_edit_or_qf,
-					["ctrl-x"] = actions.file_split,
-					["ctrl-v"] = actions.file_vsplit,
-					["ctrl-t"] = actions.file_tabedit,
+					["enter"] = with_tabufline(actions.file_edit_or_qf),
+					["ctrl-x"] = with_tabufline(actions.file_split),
+					["ctrl-v"] = with_tabufline(actions.file_vsplit),
+					["ctrl-t"] = with_tabufline(actions.file_tabedit),
 					["alt-q"] = actions.file_sel_to_qf,
 				},
 			},
